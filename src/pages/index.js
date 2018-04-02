2313789bc01e8601f6621fd64e36747795c9ac63
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2018-04-02 16:08:37 +0200

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IndexPage = () => (
   <div>
-    <h1>Awesome Collection</h1>
+    <h1>Awesome Collection of Resources</h1>
     <img
       src="https://i0.wp.com/www.theunicornsidekick.com/wp-content/uploads/2017/06/unicorn.png?resize=768%2C768"
       height="384"
